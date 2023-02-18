29bce6aa043a89e58a3e350adaf7e40bd9cfbb48
CodedRed <devcodedred@gmail.com>
2023-02-18 14:47:05 -0600

    small command optimizations

--- src/main/java/dev/codedred/safedrop/commands/Drop.java
+++ src/main/java/dev/codedred/safedrop/commands/Drop.java
@@ -10,43 +10,47 @@ import org.bukkit.entity.Player;
 
 public class Drop implements CommandExecutor {
 
+    private static final String PERMISSION_USE = "sd.use";
+    private static final String PERMISSION_ADMIN = "sd.admin";
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         DataManager dataManager = DataManager.getInstance();
         if (!(sender instanceof Player player)) {
             sender.sendMessage("[SafeDrop] Commands can only be ran in-game!");
-            return true;
+            return false;
         }
 
         if (args.length != 1) {
             sendUsage(player);
-            return true;
+            return false;
         }
 
+        DropManager dropManager = DropManager.getInstance();
         switch (args[0].toUpperCase()) {
             case "ON" -> {
-                if (player.hasPermission("sd.use")) {
-                    DropManager.getInstance().addDropStatus(player.getUniqueId(), true);
+                if (player.hasPermission(PERMISSION_USE)) {
+                    dropManager.addDropStatus(player.getUniqueId(), true);
                     player.sendMessage(ChatUtil.format(dataManager.getConfig().getString("messages.safedrop-on")));
                 } else
                     sendError(player);
             }
             case "OFF" -> {
-                if (player.hasPermission("sd.use")) {
-                    DropManager.getInstance().addDropStatus(player.getUniqueId(), false);
-                    player.sendMessage(ChatUtil.format(dataManager.getConfig().getString("messages.safedrop-on")));
+                if (player.hasPermission(PERMISSION_USE)) {
+                    dropManager.addDropStatus(player.getUniqueId(), false);
+                    player.sendMessage(ChatUtil.format(dataManager.getConfig().getString("messages.safedrop-off")));
                 } else
                     sendError(player);
             }
             case "RELOAD" -> {
-                if (player.hasPermission("sd.admin")) {
+                if (player.hasPermission(PERMISSION_ADMIN)) {
                     dataManager.reload();
                     player.sendMessage(ChatUtil.format("\n&d&lSafe Drop &dhas successfully reloaded.\n"));
                 } else
                     sendError(player);
             }
             case "REPORTBUG" -> {
-                if (player.hasPermission("sd.admin"))
+                if (player.hasPermission(PERMISSION_ADMIN))
                     player.sendMessage(ChatUtil.format("&9Report issues here: link"));
                 else
                     sendError(player);
@@ -54,17 +58,14 @@ public class Drop implements CommandExecutor {
             default -> sendUsage(player);
         }
 
-
-
-        return false;
+        return true;
     }
 
     private void sendUsage(Player player) {
         for (String msg : DataManager.getInstance().getConfig().getStringList("messages.usage"))
             player.sendMessage(ChatUtil.format(msg));
-        if (player.hasPermission("sd.admin"))
+        if (player.hasPermission(PERMISSION_ADMIN))
             player.sendMessage(ChatUtil.format("&c&lAdmin Command:\n&c/&8sd reload &7- reloads plugin\\n&c/&8sd reportbug &7- report a plugin bug"));
-
     }
 
     private void sendError(Player player) {

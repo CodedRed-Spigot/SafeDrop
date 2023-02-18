766799fe0efcec0563921ffa67660f39fff60cb7
CodedRed <devcodedred@gmail.com>
2023-02-18 14:35:40 -0600

    improved thread safety

--- src/main/java/dev/codedred/safedrop/managers/DropManager.java
+++ src/main/java/dev/codedred/safedrop/managers/DropManager.java
@@ -5,26 +5,31 @@ import dev.codedred.safedrop.data.DataManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class DropManager {
 
-	private static DropManager instance = null;
+	private static DropManager instance;
 
 	private final Map<UUID, Boolean> dropMap;
 	private final List<UUID> requestList;
 
 	public static DropManager getInstance() {
-		if (instance == null) 
+		if (instance == null)
 			instance = new DropManager();
 		return instance;
 	}
 
 	private DropManager() {
-		dropMap = new HashMap<>();
-		requestList = new ArrayList<>();
+		dropMap = new ConcurrentHashMap<>();
+		requestList = new CopyOnWriteArrayList<>();
 	}
-	
+
 	public void addRequest(UUID uuid) {
 		requestList.add(uuid);
 		startRequestDelay(uuid);
@@ -37,6 +42,7 @@ public class DropManager {
 	public boolean hasRequested(UUID uuid) {
 		return requestList.contains(uuid);
 	}
+
 	public void addDropStatus(UUID uuid, boolean drop) {
 		dropMap.put(uuid, drop);
 	}
@@ -49,15 +55,15 @@ public class DropManager {
 		return dropMap.get(uuid);
 	}
 
-
 	private void startRequestDelay(UUID uuid) {
 		DataManager dataManager = DataManager.getInstance();
 		new BukkitRunnable() {
 			int count = 0;
-			final long end = dataManager.getConfig().getLong("safe-drop.seconds") * 20L - 15L;
+			final long end = TimeUnit.SECONDS.toMillis(dataManager.getConfig().getLong("safe-drop.seconds")) - TimeUnit.SECONDS.toMillis(15L);
+
 			@Override
 			public void run() {
-				if (count >= end) {
+				if (count >= end / 1000L) {
 					if (hasRequested(uuid))
 						removeRequest(uuid);
 					cancel();

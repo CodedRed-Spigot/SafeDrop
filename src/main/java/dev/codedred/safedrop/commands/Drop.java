7f6f78995be341045476e4c256bdcabbd275035c
Matheus Colin <mattxnice@gmail.com>
2023-06-04 04:31:10 -0300

    Added MySQL support

--- src/main/java/dev/codedred/safedrop/commands/Drop.java
+++ src/main/java/dev/codedred/safedrop/commands/Drop.java
@@ -2,7 +2,7 @@ package dev.codedred.safedrop.commands;
 
 import dev.codedred.safedrop.data.DataManager;
 import dev.codedred.safedrop.managers.DropManager;
-import dev.codedred.safedrop.utils.ChatUtil;
+import dev.codedred.safedrop.utils.chat.ChatUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,30 +28,16 @@ public class Drop implements CommandExecutor {
 
         DropManager dropManager = DropManager.getInstance();
         switch (args[0].toUpperCase()) {
-            case "ON" -> {
-                if (player.hasPermission(PERMISSION_USE)) {
-                    dropManager.addDropStatus(player.getUniqueId(), true);
-                    player.sendMessage(ChatUtil.format(dataManager.getConfig().getString("messages.safedrop-on")));
-                } else
-                    sendError(player);
-            }
-            case "OFF" -> {
-                if (player.hasPermission(PERMISSION_USE)) {
-                    dropManager.addDropStatus(player.getUniqueId(), false);
-                    player.sendMessage(ChatUtil.format(dataManager.getConfig().getString("messages.safedrop-off")));
-                } else
-                    sendError(player);
-            }
             case "RELOAD" -> {
                 if (player.hasPermission(PERMISSION_ADMIN)) {
                     dataManager.reload();
-                    player.sendMessage(ChatUtil.format("\n&d&lSafe Drop &dhas successfully reloaded.\n"));
+                    player.sendMessage(ChatUtils.format("\n&d&lSafe Drop &dhas successfully reloaded.\n"));
                 } else
                     sendError(player);
             }
             case "REPORTBUG" -> {
                 if (player.hasPermission(PERMISSION_ADMIN))
-                    player.sendMessage(ChatUtil.format("&9Report issues here: link"));
+                    player.sendMessage(ChatUtils.format("&9Report issues here: link"));
                 else
                     sendError(player);
             }
@@ -63,12 +49,12 @@ public class Drop implements CommandExecutor {
 
     private void sendUsage(Player player) {
         for (String msg : DataManager.getInstance().getConfig().getStringList("messages.usage"))
-            player.sendMessage(ChatUtil.format(msg));
+            player.sendMessage(ChatUtils.format(msg));
         if (player.hasPermission(PERMISSION_ADMIN))
-            player.sendMessage(ChatUtil.format("&c&lAdmin Command:\n&c/&8sd reload &7- reloads plugin\\n&c/&8sd reportbug &7- report a plugin bug"));
+            player.sendMessage(ChatUtils.format("&c&lAdmin Command:\n&c/&8sd reload &7- reloads plugin\\n&c/&8sd reportbug &7- report a plugin bug"));
     }
 
     private void sendError(Player player) {
-        player.sendMessage(ChatUtil.format(DataManager.getInstance().getConfig().getString("messages.no-permission")));
+        player.sendMessage(ChatUtils.format(DataManager.getInstance().getConfig().getString("messages.no-permission")));
     }
 }
--- src/main/java/dev/codedred/safedrop/commands/DropCommand.java
+++ src/main/java/dev/codedred/safedrop/commands/DropCommand.java
@@ -1,8 +1,11 @@
 package dev.codedred.safedrop.commands;
 
+import dev.codedred.safedrop.SafeDrop;
 import dev.codedred.safedrop.data.DataManager;
 import dev.codedred.safedrop.managers.DropManager;
-import dev.codedred.safedrop.utils.ChatUtil;
+import dev.codedred.safedrop.model.User;
+import dev.codedred.safedrop.utils.chat.ChatUtils;
+import lombok.val;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,6 +15,12 @@ public class DropCommand implements CommandExecutor {
 
     private static final String PERMISSION_USE = "sd.use";
 
+    private SafeDrop plugin;
+
+    public DropCommand(SafeDrop plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         DataManager dataManager = DataManager.getInstance();
@@ -20,29 +29,48 @@ public class DropCommand implements CommandExecutor {
             return false;
         }
 
-        DropManager dropManager = DropManager.getInstance();
+        if (!(player.hasPermission(PERMISSION_USE)))
+            return false;
 
-        if (player.hasPermission(PERMISSION_USE)) {
-            if (!dropManager.getStatus(player.getUniqueId())) {
-                dropManager.addDropStatus(player.getUniqueId(), true);
-                player.sendMessage(ChatUtil.format(dataManager.getConfig().getString("messages.safedrop-on")));
-                return false;
-            } else
-                sendError(player);
+        val uniqueId = player.getUniqueId();
 
-            if (dropManager.getStatus(player.getUniqueId())) {
-                dropManager.addDropStatus(player.getUniqueId(), false);
-                player.sendMessage(ChatUtil.format(dataManager.getConfig().getString("messages.safedrop-off")));
-                return false;
+        val usersTable = plugin.getDatabaseManager().getUsersTable();
 
-            } else
-                sendError(player);
+        User user = usersTable.getByUuid(uniqueId);
+
+        if (user == null) {
+            User newUser = new User(player.getUniqueId(), true);
+            usersTable.insert(newUser);
+            player.sendMessage(ChatUtils.format(dataManager.getConfig().getString("messages.safedrop-on")));
+            return false;
+        }
+
+        if (user.isEnabled()) {
+            user.setEnabled(false);
+            usersTable.update(user);
+            player.sendMessage(ChatUtils.format(dataManager.getConfig().getString("messages.safedrop-off")));
+        } else {
+            user.setEnabled(true);
+            usersTable.update(user);
+            player.sendMessage(ChatUtils.format(dataManager.getConfig().getString("messages.safedrop-on")));
         }
 
         return false;
+
+/*        if (!dropManager.getStatus(player.getUniqueId())) {
+            dropManager.addDropStatus(player.getUniqueId(), true);
+            player.sendMessage(ChatUtils.format(dataManager.getConfig().getString("messages.safedrop-on")));
+            return false;
+        } else
+            sendError(player);
+
+        if (dropManager.getStatus(player.getUniqueId())) {
+            dropManager.addDropStatus(player.getUniqueId(), false);
+            player.sendMessage(ChatUtils.format(dataManager.getConfig().getString("messages.safedrop-off")));
+            return false;
+
+        } else
+            sendError(player);*/
     }
 
-    private void sendError(Player player) {
-        player.sendMessage(ChatUtil.format(DataManager.getInstance().getConfig().getString("messages.no-permission")));
-    }
 }

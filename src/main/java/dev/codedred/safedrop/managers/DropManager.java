ae6715d7ae6d1480d85be123dd65e36992f701f6
CodedRed <devcodedred@gmail.com>
2023-12-15 12:33:54 -0600

    added whitelist to drop manager

--- src/main/java/dev/codedred/safedrop/managers/DropManager.java
+++ src/main/java/dev/codedred/safedrop/managers/DropManager.java
@@ -2,68 +2,93 @@ package dev.codedred.safedrop.managers;
 
 import dev.codedred.safedrop.SafeDrop;
 import dev.codedred.safedrop.data.DataManager;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DropManager {
 
-	private static DropManager instance;
+  private static DropManager instance;
 
-	private final Map<UUID, Boolean> dropMap;
-	private final List<UUID> requestList;
+  private final Map<UUID, Boolean> dropMap;
+  private final List<UUID> requestList;
 
-	public static DropManager getInstance() {
-		if (instance == null)
-			instance = new DropManager();
-		return instance;
-	}
+  private boolean whitelistEnabled;
+  private List<String> whitelist;
 
-	private DropManager() {
-		dropMap = new ConcurrentHashMap<>();
-		requestList = new CopyOnWriteArrayList<>();
-	}
+  public static DropManager getInstance() {
+    if (instance == null) instance = new DropManager();
+    return instance;
+  }
 
-	public void addRequest(UUID uuid) {
-		requestList.add(uuid);
-		startRequestDelay(uuid);
-	}
+  private DropManager() {
+    dropMap = new ConcurrentHashMap<>();
+    requestList = new CopyOnWriteArrayList<>();
+    whitelist = new CopyOnWriteArrayList<>();
+    whitelistEnabled = false;
+  }
 
-	public void removeRequest(UUID uuid) {
-		requestList.remove(uuid);
-	}
+  public void addRequest(UUID uuid) {
+    requestList.add(uuid);
+    startRequestDelay(uuid);
+  }
 
-	public boolean hasRequested(UUID uuid) {
-		return requestList.contains(uuid);
-	}
+  public void removeRequest(UUID uuid) {
+    requestList.remove(uuid);
+  }
 
-	public void addDropStatus(UUID uuid, boolean drop) {
-		dropMap.put(uuid, drop);
-	}
+  public boolean hasRequested(UUID uuid) {
+    return requestList.contains(uuid);
+  }
 
-	public void removeDropStatus(UUID uuid) {
-		dropMap.remove(uuid);
-	}
+  public void addDropStatus(UUID uuid, boolean drop) {
+    dropMap.put(uuid, drop);
+  }
 
-	public boolean getStatus(UUID uuid) {
-		return dropMap.get(uuid);
-	}
+  public void removeDropStatus(UUID uuid) {
+    dropMap.remove(uuid);
+  }
 
-	private void startRequestDelay(UUID uuid) {
-		DataManager dataManager = DataManager.getInstance();
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				if (hasRequested(uuid))
-					removeRequest(uuid);
-				cancel();
-			}
-		}.runTaskLaterAsynchronously(JavaPlugin.getPlugin(SafeDrop.class),
-				dataManager.getConfig().getLong("safe-drop.seconds") * 20L - 15L);
-	}
+  public boolean getStatus(UUID uuid) {
+    return dropMap.get(uuid);
+  }
+
+  public void setWhitelist(List<String> whitelist) {
+    this.whitelist = new CopyOnWriteArrayList<>(whitelist);
+  }
+
+  public List<String> getWhitelist() {
+    return whitelist;
+  }
+
+  public boolean isWhitelisted(String item) {
+    return whitelist.contains(item.toUpperCase());
+  }
+
+  public void setWhitelistEnabled(boolean whitelistEnabled) {
+    this.whitelistEnabled = whitelistEnabled;
+  }
+
+  public boolean isWhitelistEnabled() {
+    return whitelistEnabled;
+  }
+
+  private void startRequestDelay(UUID uuid) {
+    DataManager dataManager = DataManager.getInstance();
+    new BukkitRunnable() {
+      @Override
+      public void run() {
+        if (hasRequested(uuid)) removeRequest(uuid);
+        cancel();
+      }
+    }
+      .runTaskLaterAsynchronously(
+        JavaPlugin.getPlugin(SafeDrop.class),
+        dataManager.getConfig().getLong("safe-drop.seconds") * 20L - 15L
+      );
+  }
 }
